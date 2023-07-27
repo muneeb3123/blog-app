f7dad3aa93ee3fbde3f676420751c5a21a08f7a8
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-07-28 01:43:48 +0500

    modify

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :user, foreign_key: 'author_id'
   belongs_to :post
 
+  after_create :update_post_comments_counte
+  after_destroy :update_post_comments_counte
+
   def update_post_comments_counter
     post.update(comments_counter: post.comments.count)
   end
