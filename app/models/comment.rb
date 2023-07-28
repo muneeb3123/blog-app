d8e4d55636c68aadccace46c28da44ce3e2cf1a1
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-07-29 02:29:57 +0500

    modify

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  after_save :update_post_comments_counte
+  after_save :update_post_comments_counter
 
   private
 
