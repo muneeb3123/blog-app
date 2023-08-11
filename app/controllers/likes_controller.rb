b3ad2b82de9fc9be7a5360f806b80b26cdba0f28
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-08-11 22:44:38 +0500

    modify

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,18 +1,18 @@
 class LikesController < ApplicationController
   def create
-    @post = Post.find(params[:post_id])
-    existing_like = @post.likes.find_by(author: current_user)
+    @target_post = Post.find(params[:post_id])
+    existing_like = @target_post.likes.find_by(author: current_user)
 
     if existing_like
-      flash[:alert] = 'You have already liked this post.'
+      flash[:alert] = 'You have already expressed your appreciation for this post.'
     else
-      @like = current_user.likes.new(post: @post)
-      if @like.save
-        flash[:notice] = 'You liked the post successfully.'
+      @new_like = current_user.likes.new(post: @target_post)
+      if @new_like.save
+        flash[:notice] = 'Youhave successfully shown your support for this post.'
       else
-        flash[:alert] = 'Something went wrong. Please try again.'
+        flash[:alert] = 'Oops, something went wrong. Please try again.'
       end
     end
-    redirect_to user_post_path(@post.author, @post)
+    redirect_to user_post_path(@target_post.author, @target_post)
   end
 end
