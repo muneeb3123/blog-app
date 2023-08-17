6ff5ff7a463269eb529dcb66d5865e6e600edce4
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-08-17 05:41:23 +0500

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # after_sign_in_path_for
+  # after_sign_out_path_for
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   root 'users#index'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
