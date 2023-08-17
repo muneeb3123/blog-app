961cce66e5dd6e1f3db4002a8095bada18540514
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-08-17 05:45:24 +0500

    modify

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :photo, :bio, :posts_counter, :email, :password)}
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :photo, :bio, :posts_counter, :email, :password, :current_password)}
   end
-  private
-
-  def current_user
-    @current_user ||= User.first
-  end
 end
