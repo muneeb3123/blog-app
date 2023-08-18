0a38892b3f00c10e5d9ee347a9d3d0473ccc5b4f
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-08-18 21:09:03 +0500

    add menthod to destroy comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: %i[create destroy]
+  # load_and_authorize_resource
+
   def new
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new
@@ -9,11 +12,20 @@ class CommentsController < ApplicationController
     @comment = @post.comments.new(comments_params)
     @comment.author = current_user
     if @comment.save
-      redirect_to user_post_path(current_user, @post), notice: 'Comment was successfully created.'
+      redirect_to user_post_path(@post.author, @post), notice: 'Comment was successfully created.'
     else
       render 'posts/show'
     end
   end
+
+  def destroy
+    @user = current_user
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    authorize! :destroy, @comment
+    @comment.destroy
+    redirect_to user_post_path(@user,@post), notice: 'Comment was successfully deleted.'
+  end  
 
   private
 
