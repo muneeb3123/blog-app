220e01203c5fd8ecf10600713493c27b3c381b99
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2023-08-19 00:50:15 -0400

    changing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,26 @@
 Rails.application.routes.draw do
-  # after_sign_in_path_for
-  # after_sign_out_path_for
+  # Rutas para la autenticación de usuarios
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
+
+  # Rutas para la aplicación web
   root 'users#index'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create, :destroy] do
       resources :comments, only: [:new, :create, :destroy]
       resources :likes, only: [:create]
+    end
+  end
 
+  # Rutas para la API v1
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show] do
+        resources :posts, only: [:index, :show] do
+          resources :comments, only: [:index, :create]
+        end
+      end
     end
   end
 end
