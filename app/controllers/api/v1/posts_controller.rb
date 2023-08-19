a8ecfefe4d76562fcf2a974c2e9ece9fa5bcb2cd
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-08-19 16:20:23 +0500

    modify

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,28 +1,11 @@
-module Api
-  module V1
-    class PostsController < ApplicationController
-      before_action :authenticate_user!
-      before_action :set_post, only: %i[show update destroy]
+class Api::V1::PostsController < ApplicationController
+  load_and_authorize_resource
 
       # GET /posts
       def index
-        user = User.find(params[:user_id])
-        posts = user.posts
+        @user = User.find(params[:user_id])
+        @posts = @user.posts
 
-        render json: posts
+        render json: @posts
       end
-
-      private
-
-      # Use callbacks to share common setup or constraints between actions.
-      def set_post
-        @post = Post.find(params[:id])
-      end
-
-      # Only allow a list of trusted parameters through.
-      def post_params
-        params.require(:post).permit(:title, :text, :author_id)
-      end
-    end
-  end
 end
